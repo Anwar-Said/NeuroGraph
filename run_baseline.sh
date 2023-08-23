409d122750044e04887f68fd79b03eb0501709fd
Anwar Said <anwar.said@itu.edu.pk>
2023-08-23 10:33:18 -0500

    Update run_baseline.sh

--- run_baseline.sh
+++ run_baseline.sh
@@ -3,4 +3,4 @@ batch_size="16"
 model="GCNConv"
 hidden="64"
 main="main.py"
-python $main --dataset $dataset --model $model --device 'cpu' --batch_size $batch_size --runs 10
+python $main --dataset $dataset --model $model --device 'cuda' --batch_size $batch_size --runs 10
