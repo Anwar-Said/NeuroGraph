95ddc1db0713d723538b233aafcc046bab4a8149
Anwar Said <anwar.said@itu.edu.pk>
2023-10-05 13:39:20 -0500

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -1,4 +1,4 @@
-# import sphinx_rtd_theme
+import sphinx_rtd_theme
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -22,7 +22,7 @@ release = '2.0.2'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["sphinx.ext.todo","sphinx.ext.viewcode","sphinx.ext.autodoc"]
-html_output_path = '$READTHEDOCS_OUTPUT/html'
+# html_output_path = '$READTHEDOCS_OUTPUT/html'
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
@@ -34,6 +34,6 @@ pygments_style = 'sphinx'
 todo_include_todos = False
 
 html_theme = 'sphinx_rtd_theme'
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_static_path = ['_static']
